0be9c443da20b80b34deb68f0ce091112af181b3
park kyung suk <boardguy1024@gmail.com>
2018-09-12 14:53:55 +0900

    firebase SDKを追加

--- app/src/main/java/com/example/parkkyungsuk/kotlinmassanger/LoginActivity.kt
+++ app/src/main/java/com/example/parkkyungsuk/kotlinmassanger/LoginActivity.kt
@@ -2,11 +2,16 @@ package com.example.parkkyungsuk.kotlinmassanger
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        textView_backToRegistration.setOnClickListener {
+            finish()
+        }
     }
 }
