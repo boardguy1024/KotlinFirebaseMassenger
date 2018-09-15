f3275ab30906e234d26f1ff92bb241568dbf4c6c
park kyung suk <boardguy1024@gmail.com>
2018-09-15 13:48:42 +0900

    firebase SDK追加しemail.passwdで登録できるように追加

--- app/src/main/java/com/example/parkkyungsuk/kotlinmassanger/MainActivity.kt
+++ app/src/main/java/com/example/parkkyungsuk/kotlinmassanger/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,11 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         registerButton_register.setOnClickListener {
 
-            val email = email_editText_registraion_screen.text.toString()
-            val password = password_editText_registraion_screen.text.toString()
-
-            Log.d("MainActivity", "email is" + email)
-            Log.d("MainActivity", "password is $password")
+           performRegister()
         }
 
         already_have_account_textView.setOnClickListener {
@@ -28,4 +26,51 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    private fun performRegister() {
+        val email = email_editText_registraion_screen.text.toString()
+        val password = password_editText_registraion_screen.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this,"Please enter your email/password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+
+        Log.d("MainActivity", "email is" + email)
+        Log.d("MainActivity", "password is $password")
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) return@addOnCompleteListener
+
+                    //else if sucessful
+                    Log.d("Main", "Successfully created user with uid ${it.result.user.uid}")
+
+                }
+                .addOnFailureListener {
+                    Log.d("Main", "${it.message}")
+                    Toast.makeText(this,"${it.message}", Toast.LENGTH_SHORT).show()
+                }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
